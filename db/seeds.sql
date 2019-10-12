a2b3d4ca752ccf5952b0eda73f0ea09dcf6e0870
Guillermo Villalta <mexcelus@gmail.com>
2019-10-12 00:01:22 -0600

    database schema and seeds created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible', false);
