978b08a854e6fc7d413d944c3f9e8501cdf229dc
Guillermo Villalta <mexcelus@gmail.com>
2019-10-12 19:24:56 -0600

    added redirect instead of responses

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -28,12 +28,7 @@ router.post("/consume/:id", (req, res) => {
     console.log(condition);
 
     burger.update(condition, () => {
-        if (res.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        };
+        res.redirect('/');
     });
     
 });
