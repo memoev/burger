8862a737c84a0b1d4cf6701c7b91f0651a41fc31
Guillermo Villalta <mexcelus@gmail.com>
2019-10-12 20:17:49 -0600

    added heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,19 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+let connection;
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'hacktheplanet',
+        database: 'todoagain_db'
+    });
+};
+
+connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
