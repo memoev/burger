9c5e744b99560e8fb6885f3a773ea666450ee6e4
Guillermo Villalta <mexcelus@gmail.com>
2019-10-12 18:51:29 -0600

    update to devour = true

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -22,5 +22,20 @@ router.post("/add", (req, res) => {
     });
 });
 
+router.post("/consume/:id", (req, res) => {
+    const condition = "id = " + req.params.id;
+
+    console.log(condition);
+
+    burger.update(condition, () => {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        };
+    });
+    
+});
 
 module.exports = router;
