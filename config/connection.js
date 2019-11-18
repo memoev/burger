481c684e922916d9794a3d28bb8c2e8e4f522538
Guillermo Villalta <mexcelus@gmail.com>
2019-11-18 11:12:21 -0700

    conneciton bug

--- config/connection.js
+++ config/connection.js
@@ -6,20 +6,13 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'hacktheplanet',
-        database: 'todoagain_db'
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "FabiolaV22!",
+        database: "burgers_db"
     });
 };
-
-connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "FabiolaV22!",
-    database: "burgers_db"
-});
 
 // Make connection.
 connection.connect(function (err) {
